bd81e36c223d6b4b954a21ed184dfdc691cb2a41
xvzhicheng <xv_zc@qq.com>
2026-05-12 18:31:47 +0800

    [feat]评论报错No procedure found on path "comment.create"；点赞无反应；

--- app/api/router.ts
+++ app/api/router.ts
@@ -3,6 +3,10 @@ import { localAuthRouter } from "./local-auth-router";
 import { dreamRouter } from "./dream-router";
 import { logRouter } from "./log-router";
 import { exploreRouter } from "./explore-router";
+import { likeRouter } from "./like-router";
+import { followRouter } from "./follow-router";
+import { commentRouter } from "./comment-router";
+import { userRouter } from "./user-router";
 import { createRouter, publicQuery } from "./middleware";
 
 export const appRouter = createRouter({
@@ -12,6 +16,10 @@ export const appRouter = createRouter({
   dream: dreamRouter,
   log: logRouter,
   explore: exploreRouter,
+  like: likeRouter,
+  follow: followRouter,
+  comment: commentRouter,
+  user: userRouter,
 });
 
 export type AppRouter = typeof appRouter;
